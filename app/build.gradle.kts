7830774abaaf0a4a0f037fc9a9c56691e8d999a7
tnxec2 <tnxec2@gmail.com>
2024-09-18 13:39:33 +0200

    1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 12
-        versionName = "1.5.0"
+        versionName = "1.5.1"
         setProperty("archivesBaseName", "$applicationId-v$versionCode($versionName)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
